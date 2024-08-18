4e67f6fece4e89edf35819eb8628f2d9f2ea1086
pramana <56497971+oklangame@users.noreply.github.com>
2024-08-18 20:27:23 +0800

    Add files via upload

---
+++ SpeedTrackerHit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedTrackerHit : MonoBehaviour
+{
+    private Vector3 lastPosition;
+    //public Transform targetPositionR;
+    public static float speedHitR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            speedHitR = (((transform.position - lastPosition).magnitude) / Time.deltaTime);
+            lastPosition = transform.position; 
+            
+    }
+}
